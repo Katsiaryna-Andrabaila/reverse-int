3cb2400a5dac488ba06fd33b528f08efa8e923d6
katsiaryna-andrabaila <androbailoe@gmail.com>
2022-06-30 21:57:33 +0300

    First attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
-}
+    let result = '';
+    let nStr = n.toString();
+    let i = nStr.length - 1;
+      
+      if (nStr[0] !== '-') {
+        while (i >= 0) {
+            result = `${result}${nStr[i]}`;
+          --i;
+        }
+        return Number(result);
+      } else {
+        while (i > 0) {
+            result = `${result}${nStr[i]}`;
+        i = i - 1;
+      }
+        return Number(result);
+      }
+  };
